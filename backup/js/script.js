c9a217e9a68b798679e248c8acef1a48b0522509
Lincoln Borges <lincolnxborges@gmail.com>
2021-05-21 18:11:43 -0300

    Upload do Projeto

---
+++ backup/js/script.js
@@ -0,0 +1,19 @@
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide") //Esconde e mostra item
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")   
+        
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")    
+
+} //Ouvi o click
+
+        
